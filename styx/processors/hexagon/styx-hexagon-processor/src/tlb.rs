7bed9c02ded7f3080c8528c2d66883c63f1fcf1c
Ramesh Balaji <me@ramesh.tv>
2026-02-15 14:19:55 -0500

    chore: remove tlb features

--- styx/processors/hexagon/styx-hexagon-processor/src/tlb.rs
+++ styx/processors/hexagon/styx-hexagon-processor/src/tlb.rs
@@ -6,11 +6,7 @@ use styx_core::{
     memory::{
         MemoryOperation, MemoryType, TlbImpl, TlbProcessor, TlbTranslateError, TlbTranslateResult,
     },
-    prelude::log::{error, info},
 };
-
-// See https://github.com/quic/qemu/blob/hex-next/target/hexagon/cpu.h.
-const MAX_TLB_ENTRIES: usize = 1024;
 
 #[bitfield(u64, debug)]
 struct Pte {
@@ -43,40 +39,28 @@ struct Pte {
     v: bool,
 }
 
-pub struct HexagonTlb {
-    entries: [Pte; MAX_TLB_ENTRIES],
-    enable_code_translation: bool,
-    enable_data_translation: bool,
-}
+pub struct HexagonTlb;
 
 impl HexagonTlb {
     pub fn new() -> Self {
-        Self {
-            enable_code_translation: false,
-            enable_data_translation: false,
-            entries: [Pte::new_with_raw_value(0); MAX_TLB_ENTRIES],
-        }
+        Self {}
     }
 }
 
 impl TlbImpl for HexagonTlb {
     fn enable_data_address_translation(&mut self) -> Result<(), UnknownError> {
-        self.enable_data_translation = true;
         Ok(())
     }
 
     fn disable_data_address_translation(&mut self) -> Result<(), UnknownError> {
-        self.enable_data_translation = false;
         Ok(())
     }
 
     fn enable_code_address_translation(&mut self) -> Result<(), UnknownError> {
-        self.enable_code_translation = true;
         Ok(())
     }
 
     fn disable_code_address_translation(&mut self) -> Result<(), UnknownError> {
-        self.enable_code_translation = false;
         Ok(())
     }
 
@@ -87,50 +71,11 @@ impl TlbImpl for HexagonTlb {
         _memory_type: MemoryType,
         _processor: &mut TlbProcessor,
     ) -> TlbTranslateResult {
-        if !self.enable_code_translation && !self.enable_data_translation {
-            Ok(virt_addr)
-        } else {
-            let virt_addr = virt_addr as u32;
-            // 4k page
-            let vpn = virt_addr >> 16;
-            let offset = virt_addr & 0xffff;
-
-            for ent in &self.entries {
-                if !ent.v() {
-                    continue;
-                }
-
-                let ppd = u64::from(ent.ppd() >> 5).overflowing_shl(16).0;
-                let ent_vpn = u32::from(ent.vpn()) >> 4;
-
-                info!("ent vpn {ent_vpn:x} real vpn {vpn:x}");
-                info!(
-                    "ent ppd {:x} shift ppd {:x} off {:x}",
-                    ent.ppd(),
-                    ppd,
-                    offset
-                );
-                if ent_vpn == vpn {
-                    let pa = ppd + offset as u64;
-                    info!("translated {virt_addr:x} to {pa:x}");
-                    return Ok(pa);
-                }
-            }
-
-            error!("couldn't translate {virt_addr:x}");
-            // lol what
-            Err(TlbTranslateError::Other(UnknownError::msg("tlb failed")))
-        }
+        // Translation is not implemented for now
+        Ok(virt_addr)
     }
 
-    fn tlb_write(&mut self, idx: usize, data: u64, _flags: u32) -> Result<(), TlbTranslateError> {
-        let pte = Pte::new_with_raw_value(data);
-        self.entries[idx] = pte;
-        info!(
-            "tlb inserted at {idx} was {pte:x?} PA 0x{:x} VA 0x{:x}",
-            (pte.ppd() >> 1).overflowing_shl(12).0,
-            pte.vpn().overflowing_shl(12).0
-        );
+    fn tlb_write(&mut self, _idx: usize, _data: u64, _flags: u32) -> Result<(), TlbTranslateError> {
         Ok(())
     }
 
@@ -138,7 +83,6 @@ impl TlbImpl for HexagonTlb {
         todo!()
     }
 
-    /// This is only used for ASID in our implementation
     fn invalidate_all(&mut self, _flags: u32) -> Result<(), UnknownError> {
         todo!()
     }
